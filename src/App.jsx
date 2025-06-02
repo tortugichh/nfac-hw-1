1267ed16ce1295586dac5e4b51353665c693213e
tortugichh <otesh0621@gmail.com>
2025-06-03 00:15:29 +0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1>Vite + React + Tailwind CSS</h1>
+      </div>  
+    </>
+  )
+}
+
+export default App
